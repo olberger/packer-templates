8803948625786d6e8ca86559613831f2f5edc600
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2016-06-15 16:20:28 +0200

    Prepare for X desktop

--- scripts/fedora/virtualbox.sh
+++ scripts/fedora/virtualbox.sh
@@ -9,7 +9,7 @@ sudo dnf -y install kernel-devel
 sudo dnf -y install make
 
 # Uncomment this if you want to install Guest Additions with support for X
-# sudo dnf -y install xorg-x11-server-Xorg
+sudo dnf -y install xorg-x11-server-Xorg
 
 sudo systemctl start dkms
 sudo systemctl enable dkms
